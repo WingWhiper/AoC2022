0a739db375005ac6e11f330623bd129ea91e968f
Joseph Sergent <77675582+JJSerg@users.noreply.github.com>
2022-12-03 20:12:10 -0600

    *Add const reference to the alphabet - can't wait to learn a better way to do this *Add functions to solve day 3 part 1 problem

--- const.py
+++ const.py
@@ -4,3 +4,6 @@ SCISSORS = 3
 WON = 6
 LOST = 0
 TIED = 3
+ITEM_PRIORITY = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j',
+                 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't',
+                 'u', 'v', 'w', 'x', 'y', 'z']
--- main.py
+++ main.py
@@ -127,13 +127,62 @@ def get_round_score(opponent_choice, choice, doCorrectly):
     return total_points
 
 
-def get_total_score(file, doCorrectly):
+def get_total_score(file, do_correctly):
     data = open(file)
     total_score = 0
     for i in data:
         choices = i.split()
-        total_score += get_round_score(choices[0], choices[1], doCorrectly)
+        total_score += get_round_score(choices[0], choices[1], do_correctly)
     return total_score
+
+
+def open_rucksack(rucksack):
+    compartment = [rucksack[:len(rucksack) // 2], rucksack[len(rucksack) // 2:]]
+    print()
+    return compartment
+
+
+def compare_compartments(rucksack):
+    compartments = open_rucksack(rucksack)
+    compartment_a = compartments[0]
+    compartment_b = compartments[1]
+    matched_items = []
+    matched_letter = ''
+    for i in compartment_a:
+        for o in compartment_b:
+            if i == o and o != matched_letter:
+                matched_items.append(i)
+                matched_letter = o
+                break
+    return matched_items
+
+
+def get_priority_value(matching_priority_list):
+    total_value = 0
+    for i in matching_priority_list:
+        print(i[0])
+        if i[0].isupper():
+            print("Is was upper")
+            total_value += 26
+
+        for o in const.ITEM_PRIORITY:
+            if i[0].lower() == o:
+                print(int(const.ITEM_PRIORITY.index(o)))
+                total_value += int(const.ITEM_PRIORITY.index(o)) + 1
+                print("total value: " + str(total_value))
+    print(total_value)
+    return total_value
+
+
+def reorganize_rucksacks(file):
+    rucksacks = open(file)
+    matched_priorities = []
+    for i in rucksacks:
+        matched_priorities.append(compare_compartments(i))
+    priority_total = get_priority_value(matched_priorities)
+    print(priority_total)
+    rucksacks.close()
+    return priority_total
 
 
 # Press the green button in the gutter to run the script.
@@ -142,7 +191,9 @@ if __name__ == '__main__':
     top_three_total = get_top_three_total('venv/AoC_D1_P1.txt')
     wrongScore = get_total_score('venv/AoC_D2_P1.txt', False)
     correctScore = get_total_score('venv/AoC_D2_P1.txt', True)
+    priority_total = reorganize_rucksacks('venv/AoC_D3_P1.txt')
     print("Day one answer 1: " + str(biggest_number))
     print("Day one answer 2: " + str(top_three_total))
     print("Day two answer 1: " + str(wrongScore))
     print("Day two answer 2: " + str(correctScore))
+    print("Day three answer 1: " + str(priority_total))
